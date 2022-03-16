0404ee85a8fa9ebff07b30e5cf4c765d59e0b124
enesonmez <enesonmezx@gmail.com>
2022-03-16 11:53:23 +0300

    histogram binaning
    bining yapıldı, console'a grafik cizdirildi.

--- main.cpp
+++ main.cpp
@@ -1,54 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
 #include "bmp.cpp"
 
 using namespace std;
 
+#define BINFORMAT "[%12.6f, %12.6f) |%9u| "
 
-int* Histogram(vector<uint8_t> data, int width, int height, int binlevel)
+
+int* Histogram(vector<uint8_t> data, int width, int height, int binlevel, int min_value, int max_value)
 {
     int ptr = 0;
     int size = width * height;
-    int* histData = new int[(int)(256/binlevel)] { 0 };
+    int* histData = new int[binlevel] { 0 };
 
     while (ptr < size)
     {
-        int h = (int)data[ptr];
-        histData[h] +=1;
+        int binselect = ((int)data[ptr]-min_value)*binlevel/(max_value-min_value);
+        /*int h = (int)data[ptr];
+        histData[h] +=1;*/
+        if (binselect < binlevel)
+            histData[binselect]++;
+        else
+            histData[binlevel-1]++;
         ptr++;
     }
     return histData;
 }
 
 
-double Mean(const int* hist, int16_t size)
+double Mean(const int* hist, int16_t size, int min_value, int max_value, int bin_width)
 {
     int N = 0;
     int sum = 0;
+    int count = 0;
 
-    for ( int i = 0; i < size; i++ )
+    for ( int i = min_value; i < max_value; i+=bin_width )
     {
-        N  += hist[i];
-        sum += hist[i] * i;
+
+        N  += hist[count];
+        sum += hist[count] * i;
+        count++;
     }
     if ( N == 0 )
         return -1.0;
     else
-        return sum / N;
+        return sum / (double)N;
 }
 
 
-double Variance(const int* hist, int16_t size)
+double Variance(const int* hist, int16_t size, int min_value, int max_value, int bin_width)
 {
     int N = 0;
-    int sum = 0;
-    double mean = Mean(hist, size);
+    double sum = 0;
+    double mean = Mean(hist, size, min_value, max_value, bin_width);
+    int count = 0;
 
-    for ( int i = 0; i < size; i++ )
+    for ( int i = min_value; i < max_value; i+=bin_width )
     {
-        N  += hist[i];
-        sum += (i-mean)*(i-mean) * hist[i];
+        N  += hist[count];
+        sum += (i-mean)*(i-mean) * hist[count];
+        count++;
     }
     if ( N == 0 )
         return -1.0;
@@ -57,9 +70,9 @@ double Variance(const int* hist, int16_t size)
 }
 
 
-double Std(const int* hist, int16_t size)
+double Std(const int* hist, int16_t size, int min_value, int max_value, int bin_width)
 {
-    double variance = Variance(hist, size);
+    double variance = Variance(hist, size, min_value, max_value, bin_width);
     if ( variance == -1.0 )
         return -1.0;
     else
@@ -107,28 +120,39 @@ int Max_Element(int* hist, int size) {
 int main()
 {
     BMP bmp("lena.bmp");
-    int8_t BIN_LEVEL = 1;
+    int8_t BIN_LEVEL = 10;
     int32_t MAX_SQUARE = 50;
-    int16_t HIST_SIZE = 256 / BIN_LEVEL;
+    int16_t HIST_SIZE = BIN_LEVEL;
     int32_t NUM_SAMPLES = bmp.bmp_info_header.width*bmp.bmp_info_header.height;
+    string binformat = BINFORMAT;
 
 
-
-    int* hist = Histogram(bmp.data,bmp.bmp_info_header.width,bmp.bmp_info_header.height,BIN_LEVEL);
+    int min_value = Min(bmp.data);
+    int max_value = Max(bmp.data);
+    int bin_width = ceil((max_value-min_value)/(double)BIN_LEVEL);
+    int* hist = Histogram(bmp.data,bmp.bmp_info_header.width,bmp.bmp_info_header.height,BIN_LEVEL,min_value,max_value);
     int32_t square_count = Max_Element(hist,HIST_SIZE) / MAX_SQUARE;
+    double mean_value = Mean(hist, HIST_SIZE, min_value, max_value, bin_width);
+    double variance_value = Variance(hist, HIST_SIZE, min_value, max_value, bin_width);
+    double std_value = Std(hist, HIST_SIZE, min_value, max_value, bin_width);
 
     cout << "# Num Samples : " << NUM_SAMPLES << endl;
-    cout << "# Min : " << Min(bmp.data) << endl;
-    cout << "# Max : " << Max(bmp.data) << endl;
-    cout << "# Mean : " << Mean(hist, HIST_SIZE) << endl;
-    cout << "# Variance : " << Variance(hist, HIST_SIZE) << endl;
-    cout << "# Standard Deviation : " << Std(hist, HIST_SIZE) << endl;
+    cout << "# Min : " << min_value << endl;
+    cout << "# Max : " << max_value << endl;
+    cout << "# Mean : " << mean_value << endl;
+    cout << "# Variance : " << variance_value << endl;
+    cout << "# Standard Deviation : " << std_value << endl;
     cout << "# each " << (char)254u <<" represents a count of " << square_count << endl;
-    cout << "# ------------------------------------------------" << endl;
+    cout << "# --------------------------------------" << endl;
 
-//    int8_t bin_width = (Max(hist,HIST_SIZE)-Min(hist,HIST_SIZE))/BIN_LEVEL;
+    for(int i=0; i< BIN_LEVEL; i++){
+        printf("[%12.6f, %12.6f) |%9u| ", min_value+i*(double)bin_width, min_value+(i+1)*(double)bin_width, hist[i]);
+        for(int isquare=0; isquare<hist[i]/square_count;isquare++) cout << (char)254u << " ";
 
+        cout << endl;
+    }
 
+    cout << "# --------------------------------------" << endl;
 
     return 0;
 }
